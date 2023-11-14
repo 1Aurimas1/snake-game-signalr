db7721d5546d147672933fdbcae9824203c65bff
1Aurimas1 <46860627+1Aurimas1@users.noreply.github.com>
2023-11-14 22:05:11 +0200

    Refactor tests to incorporate services and introduce small improvements.

--- SnakeGame.Server.Tests/GamesInMemoryTests.cs
+++ SnakeGame.Server.Tests/GamesInMemoryTests.cs
@@ -1,6 +1,8 @@
 using SnakeGame.Server.Models;
 using UnitTests.Helpers;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+using SnakeGame.Server.Services.DataServices;
 
 public class GamesInMemoryTests
 {
@@ -10,11 +12,14 @@ public class GamesInMemoryTests
         await using var context = new MockDb().CreateDbContext();
         await MockDb.SeedAsync(context);
 
-        var result = await GamesEndpoints.GetAllGames(context);
+        GameService gameService = new(context);
+
+        var result = await GamesEndpoints.GetAllGames(gameService);
 
         Assert.IsType<Ok<List<GameDto>>>(result);
 
         Assert.NotNull(result.Value);
+
         Assert.NotEmpty(result.Value);
         // TODO: add more property tests?
         Assert.Collection(
@@ -34,13 +39,23 @@ public class GamesInMemoryTests
         );
     }
 
-    [Fact]
-    public async Task GetGameReturnsGameFromDatabase()
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    public async Task GetGameReturnsGameFromDatabase(int userId, int id)
     {
         await using var context = new MockDb().CreateDbContext();
         await MockDb.SeedAsync(context);
 
-        var result = await GamesEndpoints.GetUserGame(1, 1, context);
+        var mockUserManager = MockUserManager.GetUserManager<User>();
+        mockUserManager
+            .Setup(m => m.FindByIdAsync(userId.ToString()))
+            .ReturnsAsync(context.Users.FirstOrDefault(u => u.Id == userId));
+
+        UserService userService = new(mockUserManager.Object);
+        GameService gameService = new(context);
+
+        var result = await GamesEndpoints.GetUserGame(userId, id, userService, gameService);
 
         Assert.IsType<Results<Ok<GameDto>, NotFound<CustomError>>>(result);
 
@@ -48,7 +63,7 @@ public class GamesInMemoryTests
 
         Assert.NotNull(okResult.Value);
 
-        Assert.Equal(1, okResult.Value.Id);
+        Assert.Equal(userId, okResult.Value.Id);
     }
 
     [Fact]
@@ -57,13 +72,15 @@ public class GamesInMemoryTests
         await using var context = new MockDb().CreateDbContext();
         await MockDb.SeedAsync(context);
 
-        var result = await GamesEndpoints.GetAllOpenGames(context);
+        GameService gameService = new(context);
+
+        var result = await GamesEndpoints.GetAllOpenGames(gameService);
 
         Assert.IsType<Ok<List<GameDto>>>(result);
 
         Assert.NotNull(result.Value);
-        Assert.NotEmpty(result.Value);
 
+        Assert.NotEmpty(result.Value);
         Assert.True(result.Value.All(g => g.IsOpen));
     }
 
@@ -75,35 +92,61 @@ public class GamesInMemoryTests
         await using var context = new MockDb().CreateDbContext();
         await MockDb.SeedAsync(context);
 
-        var result = await GamesEndpoints.GetAllUserGames(userId, context);
+        var mockUserManager = MockUserManager.GetUserManager<User>();
+        mockUserManager
+            .Setup(m => m.FindByIdAsync(userId.ToString()))
+            .ReturnsAsync(context.Users.FirstOrDefault(u => u.Id == userId));
+
+        UserService userService = new(mockUserManager.Object);
+        GameService gameService = new(context);
+
+        var result = await GamesEndpoints.GetAllUserGames(userId, userService, gameService);
 
         Assert.IsType<Results<Ok<List<GameDto>>, NotFound<CustomError>>>(result);
 
         var okResult = (Ok<List<GameDto>>)result.Result;
 
         Assert.NotNull(okResult.Value);
-        Assert.NotEmpty(okResult.Value);
 
+        Assert.NotEmpty(okResult.Value);
         Assert.True(okResult.Value.All(g => g.Creator.Id == userId));
     }
 
     [Theory]
-    [InlineData("Test name 3", GameMode.Solo, 1)]
-    [InlineData("Test name 4", GameMode.Duel, 2)]
+    [InlineData("Test name 3", GameMode.Solo, 1, 1)]
+    [InlineData("Test name 4", GameMode.Duel, 2, 1)]
     public async Task CreateUserGameCreatesUserGameInDatabase(
         string name,
         GameMode mode,
-        int userId
+        int userId,
+        int mapId
     )
     {
         await using var context = new MockDb().CreateDbContext();
         await MockDb.SeedAsync(context);
-        var initialGameCount = context.Games.Count();
 
-        CreateGameDto createGameDto = new(name, mode, 1);
+        var mockUserManager = MockUserManager.GetUserManager<User>();
+        mockUserManager
+            .Setup(m => m.FindByIdAsync(userId.ToString()))
+            .ReturnsAsync(context.Users.FirstOrDefault(u => u.Id == userId));
+
+        UserService userService = new(mockUserManager.Object);
+        MapService mapService = new(context);
+        GameService gameService = new(context);
+
+        CreateGameDto createGameDto = new(name, mode, mapId);
         CreateGameDtoValidator validator = new(context);
 
-        var result = await GamesEndpoints.CreateUserGame(userId, createGameDto, validator, context);
+        var initialGameCount = context.Games.Count();
+
+        var result = await GamesEndpoints.CreateUserGame(
+            userId,
+            createGameDto,
+            validator,
+            userService,
+            mapService,
+            gameService
+        );
 
         Assert.IsType<
             Results<
@@ -124,28 +167,40 @@ public class GamesInMemoryTests
         Assert.Equal(1, gameDiff);
         Assert.Equal(name, lastGame.Name);
         Assert.Equal(mode, lastGame.Mode);
-        Assert.Equal(1, lastGame.Map.Id);
+        Assert.Equal(mapId, lastGame.Map.Id);
         Assert.Equal(userId, lastGame.Creator.Id);
     }
 
     [Theory]
     [InlineData(2, 2, 1)]
-    public async Task UpdateUserGameUpdatesUserGameInDatabase(int userId, int gameId, int playerId)
+    public async Task UpdateUserGameUpdatesUserGameInDatabase(int userId, int id, int playerId)
     {
         await using var context = new MockDb().CreateDbContext();
         await MockDb.SeedAsync(context);
-        var initialPlayerCount = context.Games.Single(g => g.Id == gameId).Players.Count();
+
+        var mockUserManager = MockUserManager.GetUserManager<User>();
+        mockUserManager
+            .Setup(m => m.FindByIdAsync(It.IsAny<string>()))
+            .ReturnsAsync(
+                (string arg) => context.Users.FirstOrDefault(u => u.Id == int.Parse(arg))
+            );
+
+        UserService userService = new(mockUserManager.Object);
+        GameService gameService = new(context);
+
+        var initialPlayerCount = context.Games.Single(g => g.Id == id).Players.Count();
 
         UpdateGameDto updateGameDto = new(playerId);
         UpdateGameDtoValidator validator = new();
 
         var result = await GamesEndpoints.UpdateUserGame(
             userId,
-            gameId,
+            id,
             updateGameDto,
             validator,
-            context
-        ); // Existing game should be updated
+            userService,
+            gameService
+        );
 
         Assert.IsType<
             Results<
@@ -160,23 +215,32 @@ public class GamesInMemoryTests
 
         Assert.NotNull(okResult);
 
-        var updatedGame = context.Games.Single(g => g.Id == gameId);
+        var updatedGame = context.Games.Single(g => g.Id == id);
         var playerDiff = updatedGame.Players.Count() - initialPlayerCount;
 
         Assert.Equal(1, playerDiff);
-        Assert.Contains(playerId, updatedGame.Players.Select(p => p.Id));
+        Assert.True(updatedGame.Players.Any(p => p.Id == playerId));
     }
 
     [Theory]
     [InlineData(1, 1)]
     [InlineData(2, 2)]
-    public async Task RemoveUserGameRemovesUserGameInDatabase(int userId, int gameId)
+    public async Task RemoveUserGameRemovesUserGameInDatabase(int userId, int id)
     {
         await using var context = new MockDb().CreateDbContext();
         await MockDb.SeedAsync(context);
+
+        var mockUserManager = MockUserManager.GetUserManager<User>();
+        mockUserManager
+            .Setup(m => m.FindByIdAsync(userId.ToString()))
+            .ReturnsAsync(context.Users.FirstOrDefault(u => u.Id == userId));
+
+        UserService userService = new(mockUserManager.Object);
+        GameService gameService = new(context);
+
         var initialGameCount = context.Games.Count();
 
-        var result = await GamesEndpoints.RemoveUserGame(userId, gameId, context);
+        var result = await GamesEndpoints.RemoveUserGame(userId, id, userService, gameService);
 
         Assert.IsType<Results<NoContent, NotFound<CustomError>>>(result);
 
@@ -187,6 +251,6 @@ public class GamesInMemoryTests
         var gameDiff = initialGameCount - context.Games.Count();
 
         Assert.Equal(1, gameDiff);
-        Assert.DoesNotContain(gameId, context.Games.Select(g => g.Id));
+        Assert.False(context.Games.Any(g => g.Id == userId));
     }
 }
